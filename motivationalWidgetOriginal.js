0f1aaecf5720eda710dbc31a1c8d4d5012ba29ea
stanwissink <stanwissink01@gmail.com>
2024-06-04 22:46:42 +0200

    Update motivationalWidgetOriginal.js
    colour changes

--- motivationalWidgetOriginal.js
+++ motivationalWidgetOriginal.js
@@ -2,8 +2,8 @@
 const quotesUrl = "https://raw.githubusercontent.com/stanwissink/quotes_daily/main/quotes1.json"; // Replace this URL with your own
 
 // Customization
-const backgroundColor = new Color("#002626"); // Set your desired background color
-const fontColor = new Color("#FFC745"); // Set your desired font color
+const backgroundColor = new Color("#00192B"); // Set your desired background color
+const fontColor = new Color("#FFFFFF"); // Set your desired font color
 const fontSize = 16; // Set the font size
 const grainSize = 0.5; // Set grain size
 
